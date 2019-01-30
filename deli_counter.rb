e024141f34155b228b6000be095e703e73c47abf
Kylla Harris <kylla.harris@gmail.com>
2019-01-30 17:36:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+require "pry"
+
+def line(deli)
+  katz_deli = []
+  if deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |name, i|
+     current_line << " #{i}. #{name}"
+    end
+    puts current_line
+  end
+  #"The line is currently" + name + "."
+  #The line is currently #{name}."
+end
